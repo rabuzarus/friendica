c5f26f797ce6e42c4b031b4d6e09ce3e24be5251
Philipp Holzer <admin@philipp.info>
2019-01-03 21:11:18 +0100

    Change channel "app" to "index"

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ use Friendica\Util\LoggerFactory;
 
 require_once 'boot.php';
 
-$logger = LoggerFactory::create('app');
+$logger = LoggerFactory::create('index');
 
 // We assume that the index.php is called by a frontend process
 // The value is set to "true" by default in App
