25b9d73546a45e695ae3ea232c53b1bd20170ec5
rabuzarus <rabuzarus@t-online.de>
2019-02-13 14:04:10 +0100

    create user/community pages - move the create part to another path ('new')

--- src/Module/Identities.php
+++ src/Module/Identities.php
@@ -193,110 +193,112 @@ abstract class Identities extends BaseModule
 		}
 
 
-		$max_dailies = intval(Config::get('system', 'max_daily_registrations'));
-		if ($max_dailies) {
-			$count = DBA::count('user', ['`register_date` > UTC_TIMESTAMP - INTERVAL 1 day']);
-			if ($count >= $max_dailies) {
-				Logger::log('max daily registrations exceeded.');
-				notice(L10n::t('This site has exceeded the number of allowed daily account registrations. Please try again tomorrow.') . EOL);
-				return '';
+		if (($a->argc > 1) && ($a->argv[1] == 'new')) {
+			$max_dailies = intval(Config::get('system', 'max_daily_registrations'));
+			if ($max_dailies) {
+				$count = DBA::count('user', ['`register_date` > UTC_TIMESTAMP - INTERVAL 1 day']);
+				if ($count >= $max_dailies) {
+					Logger::log('max daily registrations exceeded.');
+					notice(L10n::t('This site has exceeded the number of allowed daily account registrations. Please try again tomorrow.') . EOL);
+					return '';
+				}
 			}
-		}
 
-		$username   = defaults($_REQUEST, 'username'  , '');
-		$email      = defaults($_REQUEST, 'email'     , '');
-		$openid_url = defaults($_REQUEST, 'openid_url', '');
-		$nickname   = defaults($_REQUEST, 'nickname'  , '');
-		$photo      = defaults($_REQUEST, 'photo'     , '');
-		$account_type = intval(defaults($_REQUEST, 'account-type', 0));
-		$page_flags = intval(defaults($_REQUEST,   'page-flags',   0));
+			$username   = defaults($_REQUEST, 'username'  , '');
+			$email      = defaults($_REQUEST, 'email'     , '');
+			$openid_url = defaults($_REQUEST, 'openid_url', '');
+			$nickname   = defaults($_REQUEST, 'nickname'  , '');
+			$photo      = defaults($_REQUEST, 'photo'     , '');
+			$account_type = intval(defaults($_REQUEST, 'account-type', 0));
+			$page_flags = intval(defaults($_REQUEST,   'page-flags',   0));
 
-//		$a = self::getApp(); Logger::log("$a->user: ".print_r($a->user, true));
-		if (Config::get('system', 'publish_all')) {
-			$profile_publish = '<input type="hidden" name="profile_publish_reg" value="1" />';
-		} else {
-			$publish_tpl = Renderer::getMarkupTemplate('profile_publish.tpl');
-			$profile_publish = Renderer::replaceMacros($publish_tpl, [
-				'$instance'     => 'reg',
-				'$pubdesc'      => L10n::t('Include your profile in member directory?'),
-				'$yes_selected' => '',
-				'$no_selected'  => ' checked="checked"',
-				'$str_yes'      => L10n::t('Yes'),
-				'$str_no'       => L10n::t('No'),
+	//		$a = self::getApp(); Logger::log("$a->user: ".print_r($a->user, true));
+			if (Config::get('system', 'publish_all')) {
+				$profile_publish = '<input type="hidden" name="profile_publish_reg" value="1" />';
+			} else {
+				$publish_tpl = Renderer::getMarkupTemplate('profile_publish.tpl');
+				$profile_publish = Renderer::replaceMacros($publish_tpl, [
+					'$instance'     => 'reg',
+					'$pubdesc'      => L10n::t('Include your profile in member directory?'),
+					'$yes_selected' => '',
+					'$no_selected'  => ' checked="checked"',
+					'$str_yes'      => L10n::t('Yes'),
+					'$str_no'       => L10n::t('No'),
+				]);
+			}
+
+			$pageset_tpl = Renderer::getMarkupTemplate('settings/pagetypes.tpl');
+
+			$pagetype = Renderer::replaceMacros($pageset_tpl, [
+				'$account_types'	=> L10n::t("Account Types"),
+				'$community'		=> L10n::t("Community Forum Subtypes"),
+				'$account_type'		=> $account_type,
+				'$type_organisation' 	=> User::ACCOUNT_TYPE_ORGANISATION,
+				'$type_news'		=> User::ACCOUNT_TYPE_NEWS,
+				'$type_community' 	=> User::ACCOUNT_TYPE_COMMUNITY,
+
+				'$account_organisation'	=> ['account-type', L10n::t('Organisation Page'), User::ACCOUNT_TYPE_ORGANISATION,
+											L10n::t('Account for an organisation that automatically approves contact requests as "Followers".'),
+											($account_type == User::ACCOUNT_TYPE_ORGANISATION)],
+
+				'$account_news'		=> ['account-type', L10n::t('News Page'), User::ACCOUNT_TYPE_NEWS,
+											L10n::t('Account for a news reflector that automatically approves contact requests as "Followers".'),
+											($account_type == User::ACCOUNT_TYPE_NEWS)],
+
+				'$account_community' 	=> ['account-type', L10n::t('Community Forum'), User::ACCOUNT_TYPE_COMMUNITY,
+											L10n::t('Account for community discussions.'),
+											($account_type == User::ACCOUNT_TYPE_COMMUNITY)],
+
+				'$page_community'	=> ['page-flags', L10n::t('Public Forum'), User::PAGE_FLAGS_COMMUNITY,
+											L10n::t('Automatically approves all contact requests.'),
+											($page_flags == User::PAGE_FLAGS_COMMUNITY)],
+
+				'$page_prvgroup' 	=> ['page-flags', L10n::t('Private Forum [Experimental]'), User::PAGE_FLAGS_PRVGROUP,
+											L10n::t('Requires manual approval of contact requests.'),
+											($page_flags == User::PAGE_FLAGS_PRVGROUP)],
+
+
 			]);
+
+			$ask_password = ! DBA::count('contact');
+
+			$tpl = Renderer::getMarkupTemplate('identities.tpl');
+
+			$arr = ['template' => $tpl];
+
+			Hook::callAll('register_form', $arr);
+
+			$tpl = $arr['template'];
+
+
+			$o = Renderer::replaceMacros($tpl, [
+				'$permonly'     => intval(Config::get('config', 'register_policy')) === self::APPROVE,
+				'$permonlybox'  => ['permonlybox', L10n::t('Note for the admin'), '', L10n::t('Leave a message for the admin, why you need this identity')],
+				'$title'        => L10n::t('Identities'),
+				'$registertext' => BBCode::convert(Config::get('config', 'register_text', '')),
+				'$namelabel'    => L10n::t('The Full Name of your new identity (e.g. Joe Smith, real or real-looking): '),
+				'$ask_password' => $ask_password,
+				'$password'    => ['password', L10n::t('Please enter your password for verification:'), '', ''],
+				'$nickdesc'     => L10n::t('Choose a profile nickname. This must begin with a text character. Your profile address of your new identity will then be "<strong>nickname@%s</strong>".', self::getApp()->getHostName()),
+				'$nicklabel'    => L10n::t('Choose a nickname: '),
+				'$photo'        => $photo,
+				'$publish'      => $profile_publish,
+				'$regbutt'      => L10n::t('Create new Identitiy'),
+				'$username'     => $username,
+				'$nickname'     => $nickname,
+				'$sitename'     => self::getApp()->getHostName(),
+				'$showtoslink'  => Config::get('system', 'tosdisplay'),
+				'$tostext'      => L10n::t('Terms of Service'),
+				'$baseurl'      => System::baseurl(),
+				'$h_descadvn'   => L10n::t('Select the account type of your new identity'),
+				'$pagetype'     => $pagetype,
+				'$account_type' => $account_type,
+				'$form_security_token'   => BaseModule::getFormSecurityToken('identities'),
+				'$explicit_content'      => Config::get('system', 'explicit_content', false),
+				'$explicit_content_note' => L10n::t('Note: This node explicitly contains adult content'),
+			]);
+
+			return $o;
 		}
-
-		$pageset_tpl = Renderer::getMarkupTemplate('settings/pagetypes.tpl');
-
-		$pagetype = Renderer::replaceMacros($pageset_tpl, [
-			'$account_types'	=> L10n::t("Account Types"),
-			'$community'		=> L10n::t("Community Forum Subtypes"),
-			'$account_type'		=> $account_type,
-			'$type_organisation' 	=> User::ACCOUNT_TYPE_ORGANISATION,
-			'$type_news'		=> User::ACCOUNT_TYPE_NEWS,
-			'$type_community' 	=> User::ACCOUNT_TYPE_COMMUNITY,
-
-			'$account_organisation'	=> ['account-type', L10n::t('Organisation Page'), User::ACCOUNT_TYPE_ORGANISATION,
-										L10n::t('Account for an organisation that automatically approves contact requests as "Followers".'),
-										($account_type == User::ACCOUNT_TYPE_ORGANISATION)],
-
-			'$account_news'		=> ['account-type', L10n::t('News Page'), User::ACCOUNT_TYPE_NEWS,
-										L10n::t('Account for a news reflector that automatically approves contact requests as "Followers".'),
-										($account_type == User::ACCOUNT_TYPE_NEWS)],
-
-			'$account_community' 	=> ['account-type', L10n::t('Community Forum'), User::ACCOUNT_TYPE_COMMUNITY,
-										L10n::t('Account for community discussions.'),
-										($account_type == User::ACCOUNT_TYPE_COMMUNITY)],
-
-			'$page_community'	=> ['page-flags', L10n::t('Public Forum'), User::PAGE_FLAGS_COMMUNITY,
-										L10n::t('Automatically approves all contact requests.'),
-										($page_flags == User::PAGE_FLAGS_COMMUNITY)],
-
-			'$page_prvgroup' 	=> ['page-flags', L10n::t('Private Forum [Experimental]'), User::PAGE_FLAGS_PRVGROUP,
-										L10n::t('Requires manual approval of contact requests.'),
-										($page_flags == User::PAGE_FLAGS_PRVGROUP)],
-
-
-		]);
-
-		$ask_password = ! DBA::count('contact');
-
-		$tpl = Renderer::getMarkupTemplate('identities.tpl');
-
-		$arr = ['template' => $tpl];
-
-		Hook::callAll('register_form', $arr);
-
-		$tpl = $arr['template'];
-
-
-		$o = Renderer::replaceMacros($tpl, [
-			'$permonly'     => intval(Config::get('config', 'register_policy')) === self::APPROVE,
-			'$permonlybox'  => ['permonlybox', L10n::t('Note for the admin'), '', L10n::t('Leave a message for the admin, why you need this identity')],
-			'$title'        => L10n::t('Identities'),
-			'$registertext' => BBCode::convert(Config::get('config', 'register_text', '')),
-			'$namelabel'    => L10n::t('The Full Name of your new identity (e.g. Joe Smith, real or real-looking): '),
-			'$ask_password' => $ask_password,
-			'$password'    => ['password', L10n::t('Please enter your password for verification:'), '', ''],
-			'$nickdesc'     => L10n::t('Choose a profile nickname. This must begin with a text character. Your profile address of your new identity will then be "<strong>nickname@%s</strong>".', self::getApp()->getHostName()),
-			'$nicklabel'    => L10n::t('Choose a nickname: '),
-			'$photo'        => $photo,
-			'$publish'      => $profile_publish,
-			'$regbutt'      => L10n::t('Create new Identitiy'),
-			'$username'     => $username,
-			'$nickname'     => $nickname,
-			'$sitename'     => self::getApp()->getHostName(),
-			'$showtoslink'  => Config::get('system', 'tosdisplay'),
-			'$tostext'      => L10n::t('Terms of Service'),
-			'$baseurl'      => System::baseurl(),
-			'$h_descadvn'   => L10n::t('Select the account type of your new identity'),
-			'$pagetype'     => $pagetype,
-			'$account_type' => $account_type,
-			'$form_security_token'   => BaseModule::getFormSecurityToken('identities'),
-			'$explicit_content'      => Config::get('system', 'explicit_content', false),
-			'$explicit_content_note' => L10n::t('Note: This node explicitly contains adult content'),
-		]);
-
-		return $o;
 	}
 }
